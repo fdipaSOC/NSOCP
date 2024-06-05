e0f84f05477fc3cbc368dae6467e86ad206a13db
estebanpaduro <esteban.paduro@gmail.com>
2024-06-04 21:48:34 -0400

    Added more details to the comments of examples 1 and 2

--- example1_dist_ellip/run_dist_ellip.m
+++ example1_dist_ellip/run_dist_ellip.m
@@ -5,18 +5,92 @@
 % [1] Alfredo Canelas, Miguel Carrasco, Julio Lopez, Esteban Paduro (2024)
 %     FDIPA-SOC: A MATLAB Package for Nonlinear Second-Order Cone Programs
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+%% Example of fdipa
 % Example 1. Minimum distance between two ellipses as presented in 
 % Examples 13.5 and 14.5 of [2]
 % [2] A. Antoniou and W.-S. Lu. Practical Optimization: Algorithms and 
 % Engineering Applications. Springer Publishing Company, Incorporated, 1st edition, 2007
 %
-% min f(x1,x2,x3,x4) 
-% st g1 in K^3, g2 in K^3
+% $$\min f(x_1,x_2,x_3,x_4) $$
+% Subject to
+%  g1 in $K^3$, g2 in $K^3$
 %
-% f(x1,x2,x3,x4)=(x1-x3)^2+(x2-x4)^2
-% g1=[1;0.5*(x(1)-1);x(2)];
-% g2=[1;-0.7071*x(3)-0.7071*x(4)+4.2426;-0.3536*x(3)+0.3536*x(4)-0.7071];
+% $$f(x_1,x_2,x_3,x_4)=(x_1-x_3)^2+(x_2-x_4)^2$$
+% $g1=[1;0.5*(x_1-1);x_2];$
+% $g2=[1;-0.7071*x_3-0.7071*x_4+4.2426;-0.3536*x_3+0.3536*x_4-0.7071];$
+%% Input Functions
+% 
+% First we need to define the objective function and its gradient 
+% as a MATLAB(R) function, i.e.,
+%
+%    function [fun,grad_f]=fun_dist_ellip(x)
+%    % Objective function for the example for distance between two ellipses
+%    % Function to minimize
+%    % f(x1,x2,x3,x4)=(x1-x3)^2+(x2-x4)^2
+%        x=x(:);
+%        fun =(x(1)-x(3))^2+(x(2)-x(4))^2;
+%        grad_f=[2*(x(1)-x(3));2*(x(2)-x(4));-2*(x(1)-x(3));-2*(x(2)-x(4))];
+%    end    
+% 
+% In equivalent way to the restrictions.
+%
+%    function [g_fun,grad_g]=g_dist_ellip(x)
+%    % constraint function for the distance between two ellipses example
+%        x= x(:);
+%        g1=[1;0.5*(x(1)-1);x(2)];
+%        g2=[1;-0.7071*x(3)-0.7071*x(4)+4.2426;...
+%            -0.3536*x(3)+0.3536*x(4)-0.7071];
+%        g_fun=[g1;g2];           
+%        grad_g1=[0 0 0 0 ; 0.5 0 0 0; 0 1 0 0];
+%        grad_g2=[0 0 0 0; 0 0 -0.7071 -0.7071;  0 0 -0.3536 0.3536];
+%        grad_g=[grad_g1;grad_g2];
+%    end                             
+%
+%% Options
+% 
+% To create and modify the options of the algorithm, we consider three 
+% different experiments with different options. In the first one we use 
+% all default options, so we can simply omit the parameter. For the 
+% second one we want to only see a final summary of the algorithm, this 
+% is done by setting |Display|  to |final| and for the Hessian approximation
+% we want to disable the Hessian update (i.e. $B^k = I$), which 
+% is done by setting the parameter |HessianApproximation| to |off|
+%
+%   myOptions = fdipa_options('Display','final','HessianApproximation','off');
+%
+% The last experiment is more involved, this time we want to only get the 
+% a brief summary of the execution, so set |Display|  to |final|. Additionally
+% we want to use a custom Hessian, ideally we would use the exact formula, 
+% but since the objective function is not positive definite, that function 
+% wont work as a matrix B^k, to solve this use add a multiple of the identity
+% matrix to the exact Hessian, which guarantees that the resulting matrix is 
+% positive definite. More precisely, if we want to use the matrix modnewton
+%
+%  mod_newton = [1 0 -1 0;0 1 0 -1;-1 0 1 0;0 -1 0 1];
+%   epsilon = min(eig(mod_newton));
+%   if epsilon <= 0.0
+%     	mod_newton = mod_newton+(abs(epsilon)+0.1)*eye(4);
+%   end
+%
+% as a custom Hessian, we have to set the option |HessianApproximation| to
+% |user-supplied| and the option |HessianFcn| to be an anonymous function 
+% with the appropriate inputs
+%
+%   @(x_new,x_old,y_new, y_old, fun,gj,hess_old) mod_newton
+%
+% Putting everything together we construct the options object using the following
+%
+%   my_options = fdipa_options('Display','final', ...
+%       'HessianApproximation','user-supplied','HessianFcn',@(x_new,x_old,y_new, y_old, fun,gj,hess_old) mod_newton);
+%
+%% Other Inputs Arguments
+%
+% As we can see in this example we have 2 cone restrictions, whereby 
+% $mj=\left[3;3\right]$.
+% Finally you need to select a starting point |x0| and, if you want, 
+% an approximation of the Lagrange multiplier |y0| . In this case we 
+% are going to used the default value of |y0| .
+% x0 = [1;0;1;5];
 
 x0 = [1;0;1;5];
 y0 = [];
--- example2_rotated_conic/run_rotated_conic.m
+++ example2_rotated_conic/run_rotated_conic.m
@@ -5,8 +5,70 @@
 % [1] Alfredo Canelas, Miguel Carrasco, Julio Lopez, Esteban Paduro (2024)
 %     FDIPA-SOC: A MATLAB Package for Nonlinear Second-Order Cone Programs
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+%% Example of fdipa
 % Example 2: Quadratic objective with rotated conic constraint
+%
+% $$\min f(x,y,s) =  s_1^2 + \cdots + s_m^2+ x^2 + y^2 - 2 s_1 s_2 + x + y +2 s_1+s_2,$$
+% Subject to
+% $$\|s\|^2 \leq 2 x y, ~x\geq 0, ~y\geq 0,$$
+%
+% The constraint can be writen as a second order cone constraint using
+%
+% $$
+% \left\| \left[ \begin{array}{c} \sqrt{2}s\\ x-y\end{array}\right] \right\| \leq x+y \Leftrightarrow 
+% \left(\begin{array}{ccc}
+% 1 & 1 & 0\\
+% 0 & 0 & \sqrt{2}I_m \\
+% 1 & -1 & 0 \\
+% \end{array}\right)
+% \left(\begin{array}{c} x\\y \\ s \end{array}\right) \in \mathcal{K}^{m+2}.
+% $$
+%
+%% Input Functions
+% 
+% First we need to define the objective function and its gradient 
+% as a MATLAB(R) function, i.e., here m is a parameter that indicates the 
+% size of the vector s
+%
+%    function [fun,grad_f]=fun_rotated_conic(x,m)
+%    % Quadratic objective function for the example with rotated conic constraint
+%        x= x(:);
+%        fun= sum(x.^2)-2*x(3)* x(4) + x(1)+x(2)+2*x(3)+x(4);
+%        grad_f=2*x+[1;1;2-2*x(4);1-2*x(3);zeros(m-2,1)] ;
+%    end
+% 
+% In equivalent way to the restrictions.
+%
+%    function [g_fun,grad_g]=g_rotated_conic(x,m)
+%    % contraint function for the rotated conic constraint
+%        x=x(:);
+%        matA = zeros(m+2,m+2);
+%        matA(1,1) = 1;
+%        matA(1,2) = 1;
+%        matA(m+2,1) = 1;
+%        matA(m+2,2) = -1;
+%        matA(2:(m+1),3:m+2) = eye(m);
+%        g_fun = matA * x;
+%        grad_g = matA;
+%    end                           
+%
+%% Options
+% 
+% To create and modify the options of the algorithm, we only want to see a 
+% final summary of the algorithm, this is done by setting |Display|  
+% to |final| and all other settings as default, which is done using
+%
+%   myOptions = fdipa_options('Display','final');
+%
+%% Other Inputs Arguments
+%
+% Since the constraint can be written as a single conical constraint, 
+% we can leave the parameter |mj = []|. 
+% Finally you need to select a starting point |x0| which is done using 
+% a construction that guarantees that the starting point is strictly feasible.
+% Lastly, for the Lagrange multiplier |y0| we the default value.
+
+
 
 seed = RandStream('mt19937ar','Seed',1);
 %m=10;
@@ -31,7 +93,6 @@ clear 'seed' 'a' 'b' 'c' 'x0' 'myoptions' 'fval' 'output'
 function [fun,grad_f]=fun_rotated_conic(x,m)
 % Quadratic objective function for the example with rotated conic constraint
     x= x(:);
-    m = length(x)-2;
     fun= sum(x.^2)-2*x(3)* x(4) + x(1)+x(2)+2*x(3)+x(4);
     grad_f=2*x+[1;1;2-2*x(4);1-2*x(3);zeros(m-2,1)] ;
 end
